3bda31706422dbc40d3def889acaebdd0615df27
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-02-12 19:44:20 -0800

    Add PurchaseInfo and HealthInfo tables/entries

---
+++ src/main/java/cycleguard/database/accessor/HealthInfoAccessor.java
@@ -0,0 +1,62 @@
+package cycleguard.database.accessor;
+
+import cycleguard.database.AbstractDatabaseUserEntry;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import cycleguard.database.accessor.HealthInfoAccessor.HealthInfo;
+
+@Configuration
+public class HealthInfoAccessor extends AbstractDatabaseAccessor<HealthInfo> {
+	private final DynamoDbTable<HealthInfo> tableInstance;
+
+	protected HealthInfoAccessor() {
+		tableInstance = getClient().table("CycleGuard-HealthInfo", TableSchema.fromBean(HealthInfo.class));
+	}
+
+	@Override
+	protected DynamoDbTable<HealthInfo> getTableInstance() {
+		return tableInstance;
+	}
+
+
+	/**
+	 * {@link DynamoDbBean} linking a username to that user's health data.
+	 *
+	 * <br>
+	 * <ul>
+	 *     <li>{@link HealthInfo#heightInches} - Height of user, in inches.</li>
+	 *     <li>{@link HealthInfo#weightPounds} - Weight of user, in pounds.</li>
+	 *     <li>{@link HealthInfo#ageYears} - Age of user, in years.</li>
+	 * </ul>
+	 */
+	@DynamoDbBean
+	public static final class HealthInfo extends AbstractDatabaseUserEntry {
+		private long heightInches, weightPounds, ageYears;
+
+		public long getHeightInches() {
+			return heightInches;
+		}
+
+		public void setHeightInches(long heightInches) {
+			this.heightInches = heightInches;
+		}
+
+		public long getWeightPounds() {
+			return weightPounds;
+		}
+
+		public void setWeightPounds(long weightPounds) {
+			this.weightPounds = weightPounds;
+		}
+
+		public long getAgeYears() {
+			return ageYears;
+		}
+
+		public void setAgeYears(long ageYears) {
+			this.ageYears = ageYears;
+		}
+	}
+}
---
+++ src/main/java/cycleguard/database/accessor/UserPurchasesAccessor.java
@@ -0,0 +1,55 @@
+package cycleguard.database.accessor;
+
+import cycleguard.database.AbstractDatabaseUserEntry;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+
+import java.util.List;
+
+@Configuration
+public class UserPurchasesAccessor extends AbstractDatabaseAccessor<UserPurchasesAccessor.PurchaseInfo> {
+	private final DynamoDbTable<PurchaseInfo> tableInstance;
+
+	protected UserPurchasesAccessor() {
+		tableInstance = getClient().table("CycleGuard-PurchaseInfo", TableSchema.fromBean(PurchaseInfo.class));
+	}
+
+	@Override
+	protected DynamoDbTable<PurchaseInfo> getTableInstance() {
+		return tableInstance;
+	}
+
+
+	/**
+	 * {@link DynamoDbBean} linking a username to that user's health data.
+	 *
+	 * <br>
+	 * <ul>
+	 *     <li>{@link PurchaseInfo#cycleCoins} - Number of CycleCoins user has.</li>
+	 *     <li>{@link PurchaseInfo#themesOwned} - List of purchased themes ["Teal", "Lime", ...].</li>
+	 * </ul>
+	 */
+	@DynamoDbBean
+	public static final class PurchaseInfo extends AbstractDatabaseUserEntry {
+		private long cycleCoins;
+		private List<String> themesOwned;
+
+		public long getCycleCoins() {
+			return cycleCoins;
+		}
+
+		public void setCycleCoins(long cycleCoins) {
+			this.cycleCoins = cycleCoins;
+		}
+
+		public List<String> getThemesOwned() {
+			return themesOwned;
+		}
+
+		public void setThemesOwned(List<String> themesOwned) {
+			this.themesOwned = themesOwned;
+		}
+	}
+}
