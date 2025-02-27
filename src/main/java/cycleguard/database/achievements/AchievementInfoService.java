b2b6401018752f6e8548cbd8830ff700593a1c1a
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-02-26 22:06:33 -0800

    Change achievement order

--- src/main/java/cycleguard/database/achievements/AchievementInfoService.java
+++ src/main/java/cycleguard/database/achievements/AchievementInfoService.java
@@ -62,68 +62,68 @@ public class AchievementInfoService {
 
 		long distance = (long)(StringDoubles.toDouble(userStats.getTotalDistance()));
 		{
-			AchievementProgress dist1 = progressMap.getOrDefault(1, new AchievementProgress());
-			AchievementProgress dist2 = progressMap.getOrDefault(2, new AchievementProgress());
-			AchievementProgress dist3 = progressMap.getOrDefault(3, new AchievementProgress());
+			AchievementProgress dist1 = progressMap.getOrDefault(2, new AchievementProgress());
+			AchievementProgress dist2 = progressMap.getOrDefault(3, new AchievementProgress());
+			AchievementProgress dist3 = progressMap.getOrDefault(4, new AchievementProgress());
 
-			dist1.setCurrentProgress(Math.min(distance, 10L));
-			dist2.setCurrentProgress(Math.min(distance, 50L));
-			dist3.setCurrentProgress(Math.min(distance, 250L));
+			dist1.setCurrentProgress(Math.min(distance, 100));
+			dist2.setCurrentProgress(Math.min(distance, 1000));
+			dist3.setCurrentProgress(Math.min(distance, 10000));
 
-			dist1.setComplete(dist1.getCurrentProgress() == 10L);
-			dist2.setComplete(dist2.getCurrentProgress() == 50L);
-			dist3.setComplete(dist3.getCurrentProgress() == 250L);
+			dist1.setComplete(dist1.getCurrentProgress() == 100);
+			dist2.setComplete(dist2.getCurrentProgress() == 1000);
+			dist3.setComplete(dist3.getCurrentProgress() == 10000);
 
-			progressMap.put(1, dist1);
-			progressMap.put(2, dist2);
-			progressMap.put(3, dist3);
+			progressMap.put(2, dist1);
+			progressMap.put(3, dist2);
+			progressMap.put(4, dist3);
 		}
 
 		long time = (long)StringDoubles.toDouble(userStats.getTotalTime());
 		{
-			AchievementProgress time4 = progressMap.getOrDefault(4, new AchievementProgress());
-			AchievementProgress time5 = progressMap.getOrDefault(5, new AchievementProgress());
-			AchievementProgress time6 = progressMap.getOrDefault(6, new AchievementProgress());
+			AchievementProgress time4 = progressMap.getOrDefault(5, new AchievementProgress());
+			AchievementProgress time5 = progressMap.getOrDefault(6, new AchievementProgress());
+			AchievementProgress time6 = progressMap.getOrDefault(7, new AchievementProgress());
 
-			time4.setCurrentProgress(Math.min(time, 1L));
-			time5.setCurrentProgress(Math.min(time, 10L));
-			time6.setCurrentProgress(Math.min(time, 100L));
+			time4.setCurrentProgress(Math.min(time, 600));
+			time5.setCurrentProgress(Math.min(time, 6000));
+			time6.setCurrentProgress(Math.min(time, 60000));
 
-			time4.setComplete(time4.getCurrentProgress() == 1L);
-			time5.setComplete(time5.getCurrentProgress() == 10L);
-			time6.setComplete(time6.getCurrentProgress() == 100L);
+			time4.setComplete(time4.getCurrentProgress() == 600);
+			time5.setComplete(time5.getCurrentProgress() == 6000);
+			time6.setComplete(time6.getCurrentProgress() == 60000);
 
-			progressMap.put(4, time4);
-			progressMap.put(5, time5);
-			progressMap.put(6, time6);
+			progressMap.put(5, time4);
+			progressMap.put(6, time5);
+			progressMap.put(7, time6);
 		}
 
 		long streak = userStats.getRideStreak();
 		{
-			AchievementProgress strk7 = progressMap.getOrDefault(7, new AchievementProgress());
-			AchievementProgress strk8 = progressMap.getOrDefault(8, new AchievementProgress());
-			AchievementProgress strk9 = progressMap.getOrDefault(9, new AchievementProgress());
+			AchievementProgress strk7 = progressMap.getOrDefault(8, new AchievementProgress());
+			AchievementProgress strk8 = progressMap.getOrDefault(9, new AchievementProgress());
+			AchievementProgress strk9 = progressMap.getOrDefault(10, new AchievementProgress());
 
-			strk7.setCurrentProgress(Math.min(streak, 5L));
-			strk8.setCurrentProgress(Math.min(streak, 20L));
-			strk9.setCurrentProgress(Math.min(streak, 50L));
+			strk7.setCurrentProgress(Math.min(streak, 7));
+			strk8.setCurrentProgress(Math.min(streak, 30));
+			strk9.setCurrentProgress(Math.min(streak, 365));
 
-			strk7.setComplete(strk7.getCurrentProgress() == 5L);
-			strk8.setComplete(strk8.getCurrentProgress() == 20L);
-			strk9.setComplete(strk9.getCurrentProgress() == 50L);
+			strk7.setComplete(strk7.getCurrentProgress() == 7);
+			strk8.setComplete(strk8.getCurrentProgress() == 30);
+			strk9.setComplete(strk9.getCurrentProgress() == 365);
 
-			progressMap.put(7, strk7);
-			progressMap.put(8, strk8);
-			progressMap.put(9, strk9);
+			progressMap.put(8, strk7);
+			progressMap.put(9, strk8);
+			progressMap.put(10, strk9);
 		}
 
 		{
-			AchievementProgress all = progressMap.getOrDefault(10, new AchievementProgress());
-			if (distance>=250 && time>=100 && streak>=50) {
+			AchievementProgress all = progressMap.getOrDefault(1, new AchievementProgress());
+			if (distance>=10000 && time>=60000 && streak>=365) {
 				all.setComplete(true);
 				all.setCurrentProgress(1);
 			}
-			progressMap.put(10, all);
+			progressMap.put(1, all);
 		}
 
 		achievementInfoAccessor.setEntry(achievementInfo);
