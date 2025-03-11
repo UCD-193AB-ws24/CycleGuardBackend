05031bbd1f6bd77baeb6b3eed91121beb740a518
sravyakota20 <sskota@ucdavis.edu>
2025-03-11 17:35:18 -0400

    Moved GetUserStatus to rest package

--- src/main/java/cycleguard/rest/user/GetUserStats.java
+++ src/main/java/cycleguard/rest/user/GetUserStats.java
@@ -9,19 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Endpoint for a user to enter in health metrics.
  * Requires {@link HealthInfo} as body.
  */
 @RestController
+@RequestMapping("/user")
 public final class GetUserStats {
 	@Autowired
 	private AccessTokenManager accessTokenManager;
 	@Autowired
 	private UserStatsService userStatsService;
 
-	@GetMapping("/user/getStats")
+	@GetMapping("/getStats")
 	public UserStats getStats(@RequestHeader("Token") String token, HttpServletResponse response) {
 		String username = accessTokenManager.getUsernameFromToken(token);
 		if (username == null) {
