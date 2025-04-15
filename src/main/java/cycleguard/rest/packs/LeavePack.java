4dc43b67ea8d514ae7f2c72a2370d891fed3b63b
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-04-15 11:55:55 -0700

    Add PackData endpoints

---
+++ src/main/java/cycleguard/rest/packs/LeavePack.java
@@ -0,0 +1,36 @@
+package cycleguard.rest.packs;
+
+import cycleguard.auth.AccessTokenManager;
+import cycleguard.database.accessor.HealthInfoAccessor.HealthInfo;
+import cycleguard.database.packs.packData.PackDataService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Endpoint for a user to enter in health metrics.
+ * Requires {@link HealthInfo} as body.
+ */
+@RestController
+public final class LeavePack {
+	@Autowired
+	private AccessTokenManager accessTokenManager;
+	@Autowired
+	private PackDataService packDataService;
+
+	@PostMapping("/packs/leavePackAsOwner")
+	public void leavePackAsOwner(@RequestHeader("Token") String token, HttpServletResponse response) {
+		String username = accessTokenManager.getUsernameFromToken(token);
+		if (username == null) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
+
+		int status = packDataService.leavePack(username);
+		response.setStatus(status);
+	}
+}
